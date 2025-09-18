d8062f2e44de6ac2c4e33c028ed5ff2e090b73d9
Canmi <9997200@qq.com>
2025-09-18 19:42:20 +0800

    feat: add initial client-side App component

---
+++ src/client.tsx
@@ -0,0 +1,13 @@
+/* src/client.tsx */
+
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-[var(--color-bg)]">
+      <h1 className="text-4xl font-bold text-[var(--color-text)]">
+        Hello, World!
+      </h1>
+    </div>
+  )
+}
+
+export default App
