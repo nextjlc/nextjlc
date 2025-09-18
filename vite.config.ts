271c609f37a56d0d7b7e9b734e2945e529a770b8
Canmi <9997200@qq.com>
2025-09-18 20:25:32 +0800

    feat: add git commit hash to vite build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 /* vite.config.ts */
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { execSync } from "child_process";
+
+const gitCommitHash = execSync("git rev-parse --short HEAD").toString().trim();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    __GIT_HASH__: JSON.stringify(gitCommitHash),
+  },
+});
