da2311117c09a13ff60bb421856d5a455d136c13
Canmi <9997200@qq.com>
2025-09-18 23:45:29 +0800

    chore: Add wasm plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { execSync } from "child_process";
+import wasm from "vite-plugin-wasm";
 
 const gitCommitHash = execSync("git rev-parse --short HEAD").toString().trim();
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), wasm()],
   define: {
     __GIT_HASH__: JSON.stringify(gitCommitHash),
   },
