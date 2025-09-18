54f2164488d76862a665704969f3fdb03f299ba7
Canmi <9997200@qq.com>
2025-09-18 20:25:31 +0800

    refactor: update main.tsx to use modern React syntax

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 /* src/main.tsx */
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './client.tsx'
-import './style.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./client.tsx";
+import "./style.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
