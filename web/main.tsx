bc496dbee0f0f4c408b92da9a6ff096fd90dc9c0
Canmi <9997200@qq.com>
2026-01-16 23:01:04 +0800

    feat: Initialize React application with StrictMode and global styles

---
+++ web/main.tsx
@@ -0,0 +1,12 @@
+/* web/main.tsx */
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./client.tsx";
+import "./style.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
