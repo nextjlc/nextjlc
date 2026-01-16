e5239c1d9d500ec229bb2a272d9b06e005bb43e4
Canmi <9997200@qq.com>
2026-01-16 23:01:03 +0800

    feat: Implement main application component with workflow and progress bar

---
+++ web/client.tsx
@@ -0,0 +1,23 @@
+/* web/client.tsx */
+
+import Footer from "./footer.tsx";
+import UploadZone from "./upload.tsx";
+import Workflow from "./workflow.tsx";
+import ProgressBar from "./progress-bar.tsx";
+import { useWorkflowStore } from "./editfile";
+
+function App() {
+  const { workflowState } = useWorkflowStore();
+
+  return (
+    <main className="min-h-[100dvh] flex flex-col">
+      <ProgressBar />
+      <div className="flex-1 flex justify-center items-center p-4">
+        {workflowState === "upload" ? <UploadZone /> : <Workflow />}
+      </div>
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
